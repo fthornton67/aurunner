6fb5ef4ccd6320cb92d0a77e0febffbdf49a448e
fthornton67 <fthornton67@gmail.com>
2015-11-08 20:19:26 -0800

    look GATT Service

--- Aurunner_android/app/src/main/java/aurunner/phitr/us/aurunner/MainActivity.java
+++ Aurunner_android/app/src/main/java/aurunner/phitr/us/aurunner/MainActivity.java
@@ -2,10 +2,13 @@ package aurunner.phitr.us.aurunner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -35,6 +38,27 @@ public class MainActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_devices = (Button)findViewById(R.id.btn_devices);
+
+        btn_devices.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Log.i("****Click*****","&&&&&&&&Click&&&&&&&&");
+                Intent intent= new Intent(MainActivity.this, DeviceControlActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+
+            }
+        });
+
+        // Use this check to determine whether BLE is supported on the device. Then
+// you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
                 REDIRECT_URI);
@@ -60,7 +84,8 @@ public class MainActivity extends Activity implements
                         mPlayer.addConnectionStateCallback(MainActivity.this);
                         mPlayer.addPlayerNotificationCallback(MainActivity.this);
                         //mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
-                        mPlayer.play("spotify:track:23kyUPJdiHQwmAH4NfCGvf");
+                        //mPlayer.play("spotify:track:23kyUPJdiHQwmAH4NfCGvf");
+                        mPlayer.play("spotify:user:spotify:playlist:6VXeTHZPxzx3SGJvHJj80n");
                     }
 
                     @Override
